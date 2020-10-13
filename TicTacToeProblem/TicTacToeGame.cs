3181c6e12752ba50fcbcd49b1743d11be0b00389
piyush12346789 <piyushgupta024@gmail.com>
2020-10-13 09:47:44 +0530

    [Piyush] Add: Creating a TicTacToe board.

---
+++ TicTacToeProblem/TicTacToeGame.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TicTacToeProblem
+{
+    public class TicTacToeGame
+    {
+        static void Main(string[] args)
+        {
+            char[] board = createTicTacToeBoard();
+        }
+        private static char[] createTicTacToeBoard()
+        {
+            char[] board = new char[10];
+            for (int i = 1; i < board.Length; i++)
+            {
+                board[i] = ' ';
+            }
+            return board;
+        }
+    }
+}
